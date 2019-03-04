0c66ece24a56c890d2924bf37c83214110beb933
anisb <anisbeyzaee@Gmail.com>
2019-03-04 08:41:03 -0800

    First commit. Service created with timer, eventlog and status

--- AJEFDService3/Program.cs
+++ AJEFDService3/Program.cs
@@ -17,7 +17,7 @@ namespace AJEFDService3
             ServiceBase[] ServicesToRun;
             ServicesToRun = new ServiceBase[]
             {
-                new Service1()
+                new MyService()
             };
             ServiceBase.Run(ServicesToRun);
         }
